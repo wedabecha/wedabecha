61ac27909c519cd57d9af1cda1ff84e9248b348c
Dominic Hopf <dmaphy@googlemail.com>
2006-06-03 14:14:57 +0000

    fixed errors

--- src/de/codeforum/wedabecha/system/GraphicExport.java
+++ src/de/codeforum/wedabecha/system/GraphicExport.java
@@ -1,29 +1,44 @@
+/****************************************************************************
+ *   Copyright (C) 2004 by BTU SWP GROUP 04/6.1                             *
+ *                                                                          *
+ *   This program is free software; you can redistribute it and/or modify   *
+ *   it under the terms of the GNU General Public License as published by   *
+ *   the Free Software Foundation; either version 2 of the License, or	    *
+ *   (at your option) any later version                                     *
+ *                                                                          *
+ *   This program is distributed in the hope that it will be useful,        *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *   GNU General Public License for more details                            *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with this program; if not, write to the                          *
+ *   Free Software Foundation, Inc.,                                        *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.              *
+ ***************************************************************************/
+
 package de.codeforum.wedabecha.system;
-
-
-/**
-	@author
-		Martin Müller (mrtnmueller at users.berlios.de)
-*/
-
 // Diese Klasse dient zum Erstellen und Speichern eines Screenshots
 
 
 import com.sun.image.codec.jpeg.JPEGCodec;
 
-import de.codeforum.wedabecha.ui.MainWindow;
-
 import java.awt.*;
 import java.awt.image.*;
 import java.io.*;
 
+import de.codeforum.wedabecha.ui.MainWindow;
+import de.codeforum.wedabecha.ui.dialogs.GraphicExportUI;
 
-
+/**
+ * @author
+ * Martin Müller (mrtnmueller at users.berlios.de)
+ */
 public class GraphicExport {
 	private String dateiname;
 
 
-	public void export() {
+	public void doExport() {
 
 		//holt die Größe des Bildes aus HauptFensterUI
 		Dimension size  = MainWindow.layeredPane.getSize();
@@ -44,29 +59,29 @@ public class GraphicExport {
 				JPEGCodec.createJPEGEncoder( out ).encode( (BufferedImage)image );
 			} 	//falls Codierfehler auftreten
 				catch (IOException ochnoe){
-				GraphicExport.showEncodeError();
+				GraphicExportUI.showEncodeError();
 			} // try
 
 			try {
 				//schließt die Datei und fängt Fehler beim schließen ab
 				out.close(); } catch (IOException sonmist){
-				GraphicExport.showCouldNotCloseError();
+				GraphicExportUI.showCouldNotCloseError();
 			}
 		} catch
 			//fängt die FileNotFoundExeption ab
 			(FileNotFoundException dateinichda){
-			GraphicExport.showFileNotFoundError(this.dateiname);
+			GraphicExportUI.showFileNotFoundError(this.dateiname);
 		} // try
-	} // export()
+	} // doExport()
 
 
-	protected void setFile(String name){
+	public void setFilename(String name){
 		this.dateiname = name;
-	} // setFile()
+	} // setFilename()
 
 
-	protected String getFile(){
+	public String getFilename(){
 		return this.dateiname;
-	}//getFile()
+	} // getFilename()
 
 } // exportiereGrafik
