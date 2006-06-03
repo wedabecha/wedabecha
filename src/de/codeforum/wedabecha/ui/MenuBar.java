cb910a33be720f9dce8920f028a269dc2b661012
Dominic Hopf <dmaphy@googlemail.com>
2006-06-03 13:53:21 +0000

    rewrote the MenuBar.

--- src/de/codeforum/wedabecha/ui/MenuBar.java
+++ src/de/codeforum/wedabecha/ui/MenuBar.java
@@ -43,14 +43,22 @@ public class MenuBar extends JMenuBar {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	// alle Bestandteile des Menüs als Klassenatrribute deklarieren
+	private boolean curveEditable[];
 	
-	// konstruktor
+	/**
+	 * Konstruktor, erwartet keine weiteren Parameter
+	 */
 	public MenuBar(){
 		this.pack();
 	} // hauptMenuUI
-
+	
+	
+	/**
+	 * setzt die Menuleiste zusammen. Wird vom Konstruktor aufgerufen.
+	 */
 	public void pack(){
+		// begin fileMenu
+		
 		final JMenu fileMenu = new JMenu("Datei");
 		
 		// Items for fileMenu
@@ -106,86 +114,7 @@ public class MenuBar extends JMenuBar {
 				wedabecha.QuitProgram();
 			}
 		});
-
-		/*
-			Menu [Kurve] : hat fünf einträge, für maximal fünf kurven,
-			also machen wir ne liste von einträgen für das menu,
-			das können wir per for-schleife besser packen,
-			und eine methode einfacher schreiben, mit denen wir die
-			Menueinträge schneller umd mit weniger code aktiveren und deaktivieren können...
-		*/
-		final JMenu kurveMenu = new JMenu("Kurve");
-			final JMenu kurveMenuList[] = {
-				new JMenu("Kurve 1"),
-				new JMenu("Kurve 2"),
-				new JMenu("Kurve 3"),
-				new JMenu("Kurve 4"),
-				new JMenu("Kurve 5")
-			};
-
-/*
-			final JMenuItem kurveOeffnenMenuItem[] = {
-				new JMenuItem("Oeffnen"),
-				new JMenuItem("Oeffnen"),
-				new JMenuItem("Oeffnen"),
-				new JMenuItem("Oeffnen"),
-				new JMenuItem("Oeffnen")
-			};
-*/
-
-/*
-			final JMenuItem kurveSpeichernMenuItem[] = {
-				new JMenuItem("Speichern"),
-				new JMenuItem("Speichern"),
-				new JMenuItem("Speichern"),
-				new JMenuItem("Speichern"),
-				new JMenuItem("Speichern")
-			};
-*/
-			
-			final JMenuItem kurveDarstellungMenuItem[] = {
-				new JMenuItem("Darstellung"),
-				new JMenuItem("Darstellung"),
-				new JMenuItem("Darstellung"),
-				new JMenuItem("Darstellung"),
-				new JMenuItem("Darstellung")
-			};
-
-			final JMenuItem kurveNeuZeichnenMenuItem[] = {
-				new JMenuItem("Neu Zeichnen"),
-				new JMenuItem("Neu Zeichnen"),
-				new JMenuItem("Neu Zeichnen"),
-				new JMenuItem("Neu Zeichnen"),
-				new JMenuItem("Neu Zeichnen")
-			};
-
-			final int kurveIt;
-
-/*			
-		final JMenu annotationMenu = new JMenu("Annotation");
-			final JMenuItem annotationPfeilMenuItem = new JMenuItem("Pfeil zeichnen");
-			final JMenuItem annotationLinieMenuItem = new JMenuItem("Linie zeichnen");
-			final JMenuItem annotationTextMenuItem = new JMenuItem("Text einf\u00fcgen");
-*/
-//		final JMenu bearbeitenMenu = new JMenu("Bearbeiten");
-
-/*			
-		final JMenu ansichtMenu = new JMenu("Ansicht");
-			final JCheckBoxMenuItem zeigeGitterMenuItem = new JCheckBoxMenuItem("Gitter anzeigen");
-			final JCheckBoxMenuItem zeigeToolBarItem = new JCheckBoxMenuItem("Werkzeugleiste anzeigen");
-*/
-		final JMenu hilfeMenu = new JMenu("Hilfe");
-			final JMenuItem kurzAnleitungMenuItem = new JMenuItem("Kurzanleitung");
-			final JMenuItem dokuMenuItem = new JMenuItem("Dokumentation");
-			final JMenuItem aboutMenuItem = new JMenuItem("\u00dcber");
-
-		/**
-			Die Methode setzt das HauptMenue aus den Klassenattributen zusammen.
-		*/
-		// die elemente des menues werden "rückwärts" zusammengesetzt
-		// d.h. erst setze ich das dateimenü zusammen, bevor ich es zur MenuBar adde usw.
-
-		// begin fileMenu
+		
 		fileMenu.add(I_openFile);
 		fileMenu.addSeparator();
 		fileMenu.add(I_DataImport);
@@ -197,180 +126,149 @@ public class MenuBar extends JMenuBar {
 		fileMenu.addSeparator();
 		fileMenu.add(I_QuitProgram);
 		this.add(fileMenu);
-		// end fileMenu
+		// fileMenu END
 
-		// beginn kurve-menues
-			for (kurveIt = 0; kurveIt < 5; kurveIt++){
-				kurveMenu.add(kurveMenuList[kurveIt]);
-// 					kurveMenuList[kurveIt].add(kurveOeffnenMenuItem[kurveIt]);
-// 						kurveOeffnenMenuItem[kurveIt].setEnabled(false);
-// 					kurveMenuList[kurveIt].add(kurveSpeichernMenuItem[kurveIt]);
-// 						kurveSpeichernMenuItem[kurveIt].setEnabled(false);
-					kurveMenuList[kurveIt].add(kurveDarstellungMenuItem[kurveIt]);
-						kurveDarstellungMenuItem[kurveIt].setEnabled(false);
-					kurveMenuList[kurveIt].add(kurveNeuZeichnenMenuItem[kurveIt]);
-						kurveNeuZeichnenMenuItem[kurveIt].setEnabled(false);
-						kurveNeuZeichnenMenuItem[kurveIt].addActionListener(
-							new ActionListener(){
-								public void actionPerformed(ActionEvent event){
-									int datenLaengen[] = new int[5];
-
-									for(int i = 1; i <= 5; i++){
-							    		// alle importierten Tabellen als Kurve zeichnen
-										if(wedabecha.getCurve(i).isset()){
-
-											try{
-												MainWindow.layeredPane.remove(
-													MainWindow.layeredPane.getIndexOf(
-														wedabecha.getCurve(i).zeichneAktienKurve
-													)
-												);
-
-											} catch (ArrayIndexOutOfBoundsException except){
-												try{
-													MainWindow.layeredPane.remove(
-														MainWindow.layeredPane.getIndexOf(
-															wedabecha.getCurve(i).zeichneLinienKurve
-														)
-													);
-
-												} catch(ArrayIndexOutOfBoundsException exception){
-													// mache von mir aus nix
-												} // try
-
-											} // try
-
-
-											MainWindow.layeredPane.repaint();
-
-											if(wedabecha.getCurve(1).isset()){
-												wedabecha.getCurve(1).zeichneKurve();
-												MainWindow.toolBar.setKurve1Button();
-											}//if
-
-											if(wedabecha.getCurve(2).isset()){
-												wedabecha.getCurve(2).zeichneKurve();
-												MainWindow.toolBar.setKurve2Button();
-											}//if
-
-											if(wedabecha.getCurve(3).isset()){
-												wedabecha.getCurve(3).zeichneKurve();
-												MainWindow.toolBar.setKurve3Button();
-											}//if
-
-											if(wedabecha.getCurve(4).isset()){
-												wedabecha.getCurve(4).zeichneKurve();
-												MainWindow.toolBar.setKurve4Button();
-											}//if
-
-											if(wedabecha.getCurve(5).isset()){
-												wedabecha.getCurve(5).zeichneKurve();
-												MainWindow.toolBar.setKurve5Button();
-											}//if
-
-											datenLaengen[i] = wedabecha.getCurve(i).getDaten().size();
-
-										} // if
-
-										java.util.Arrays.sort(datenLaengen);
-										MainWindow.maxDate = datenLaengen[4];
-									} // for
-
-									MainWindow.layeredPane.repaint();
-								} // actionPerformed()
-							} // ActionListener()
-						);
-
-			} // for
-
-			kurveDarstellungMenuItem[0].addActionListener(new ActionListener(){
-				public void actionPerformed(ActionEvent event){
-					new CurveType(1);
+		// curveMenu
+		final JMenu curveMenu = new JMenu("Kurve");
+		
+		for (int i = 1; i < this.curveEditable.length; i++) {
+			final int currentCurve = i;
+			
+			JMenu I_curveMenu = new JMenu("Kurve " + currentCurve);
+			
+			JMenuItem I_openCurve = new JMenuItem("Oeffnen");
+			I_openCurve.setEnabled(false);
+			I_curveMenu.add(I_openCurve);
+			
+			JMenuItem I_saveCurve = new JMenuItem("Speichern");
+			I_saveCurve.setEnabled(false);
+			I_curveMenu.add(I_saveCurve);
+			
+			JMenuItem I_curveGraph = new JMenuItem("Darstellung");
+			I_curveGraph.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent event) {
+					new CurveType(currentCurve);
 				}
 			});
-
-			kurveDarstellungMenuItem[1].addActionListener(new ActionListener(){
+			I_curveMenu.add(I_curveGraph);
+			
+			JMenuItem I_curveRedraw = new JMenuItem("Neu Zeichnen");
+			I_curveRedraw.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent event){
-					new CurveType(2);
-				}
-			});
+					// alle importierten Tabellen als Kurve zeichnen
+					if (wedabecha.getCurve(currentCurve).isset()) {
+						try {
+							MainWindow.layeredPane.remove(
+									MainWindow.layeredPane.getIndexOf (
+											wedabecha.getCurve(currentCurve).zeichneAktienKurve
+									)
+							);
+						} catch (ArrayIndexOutOfBoundsException except){
+							try {
+								MainWindow.layeredPane.remove(
+										MainWindow.layeredPane.getIndexOf (
+												wedabecha.getCurve(currentCurve).zeichneLinienKurve
+										)
+								);
+							} catch (ArrayIndexOutOfBoundsException exception) {
+								// mache von mir aus nix
+							} // try
+						} // try
+	
+						MainWindow.layeredPane.repaint();
+						
+						wedabecha.getCurve(currentCurve).zeichneKurve();
+						MainWindow.toolBar.setKurve1Button();
+					} // if
 
-			kurveDarstellungMenuItem[2].addActionListener(new ActionListener(){
-				public void actionPerformed(ActionEvent event){
-					new CurveType(3);
-				}
-			});
+					/*
+					java.util.Arrays.sort(datenLaengen);
+					MainWindow.maxDate = datenLaengen[4];
+					*/
+					
+					MainWindow.layeredPane.repaint();
+				} // actionPerformed()
+			} // ActionListener()
+			);
 
-			kurveDarstellungMenuItem[3].addActionListener(new ActionListener(){
-				public void actionPerformed(ActionEvent event){
-					new CurveType(4);
-				}
-			});
+			I_curveMenu.add(I_curveRedraw);
+				
+			curveMenu.add(I_curveMenu);			
+		} // for
+		// curveMenu END
+		
+		// annotationMenu
+		final JMenu annotationMenu = new JMenu("Annotation");
+		
+		final JMenuItem I_curveAnnotationArrow = new JMenuItem("Pfeil zeichnen");
+		I_curveAnnotationArrow.setEnabled(false);
+		annotationMenu.add(I_curveAnnotationArrow);
+		
+		final JMenuItem I_curveAnnotationLine = new JMenuItem("Linie zeichnen");
+		I_curveAnnotationLine.setEnabled(false);
+		annotationMenu.add(I_curveAnnotationLine);
+		
+		final JMenuItem I_curveAnnotationText = new JMenuItem("Text einf\u00fcgen");
+		I_curveAnnotationText.setEnabled(false);
+		annotationMenu.add(I_curveAnnotationText);
+		
+		this.add(annotationMenu);
+		// annotationMenu END
+	
+		// editMenu
+		final JMenu editMenu = new JMenu("Bearbeiten");
+		this.add(editMenu);
+		// editMenu END
+		
+		// viewMenu
+		final JMenu viewMenu = new JMenu("Ansicht");
+		
+		final JCheckBoxMenuItem I_showGrid = new JCheckBoxMenuItem("Gitter anzeigen");
+		I_showGrid.setEnabled(false);
+		viewMenu.add(I_showGrid);
+		
+		final JCheckBoxMenuItem I_showToolbar = new JCheckBoxMenuItem("Werkzeugleiste anzeigen");
+		I_showToolbar.setEnabled(false);
+		viewMenu.add(I_showToolbar);
+		// viewMenu END
 
-			kurveDarstellungMenuItem[4].addActionListener(new ActionListener(){
-				public void actionPerformed(ActionEvent event){
-					new CurveType(5);
-				}
-			});
+		final JMenu helpMenu = new JMenu("Hilfe");
+		final JMenuItem I_shortDoc = new JMenuItem("Kurzanleitung");
+		I_shortDoc.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				new Documentation("Kurzanleitung");
+			}
+		});
+		helpMenu.add(I_shortDoc);
+		final JMenuItem I_Doc = new JMenuItem("Dokumentation");
+		I_Doc.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				new Documentation("Dokumentation");
+			}
+		});
+		helpMenu.add(I_Doc);
+		helpMenu.addSeparator();
+		
+		final JMenuItem I_about = new JMenuItem("\u00dcber");
+		I_about.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent event){
+				new About();
+			}
+		});
 
-		this.add(kurveMenu);
-		// ende kurve-menues
-
-
-		// beginn annotationMenu
-// 			annotationMenu.add(annotationPfeilMenuItem);
-// 			annotationMenu.add(annotationLinieMenuItem);
-// 			annotationMenu.add(annotationTextMenuItem);
-// 		mainMenuBar.add(annotationMenu);
-		// ende annotationMenu
-
-		// beginn bearbeitenMenu
-// 		mainMenuBar.add(bearbeitenMenu);
-		// ende bearbeitenMenu
-
-		/*
-			wird vielleicht noch Implementiert werden
-		*/
-
-		// beginn ansichtMenu
-// 		ansichtMenu.add(zeigeGitterMenuItem);
-// 			zeigeGitterMenuItem.addActionListener(new zeichneRasterListener());
-// 		ansichtMenu.add(zeigeToolBarItem);
-// 			zeigeToolBarItem.addActionListener(new zeigeToolBarListener());
-// 		mainMenuBar.add(ansichtMenu);
-		// ende ansichtMenu
-
-		// beginn hilfeMenu
-			hilfeMenu.add(kurzAnleitungMenuItem);
-				kurzAnleitungMenuItem.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent event) {
-						new Documentation("Kurzanleitung");
-					}
-				});
-			hilfeMenu.add(dokuMenuItem);
-				dokuMenuItem.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent event) {
-						new Documentation("Dokumentation");
-					}
-				});
-			hilfeMenu.addSeparator();
-			hilfeMenu.add(aboutMenuItem);
-				aboutMenuItem.addActionListener(new ActionListener(){
-					public void actionPerformed(ActionEvent event){
-						new About();
-					}
-				});
-		this.add(hilfeMenu);
-		// ende hilfeMenu
 	} // pack()
-
-	public void setKurveEditable(int kurvenNummer, boolean wasnjetz){
+	
+	/**
+	 * set a Curve in mode editable or not editable
+	 * @param id Die ID der Kurve, deren Status gesetzt werden soll.
+	 * @param flag true oder false für "bearbeitbar" oder "nicht bearbeitbar"
+	 */
+	public void setCurveEditable(int id, boolean flag){
 		/**
 			setKurveEditable setzt die einzelnen Menueinträge für eine Kurve
 			aktiv oder deaktiv... wird z.b. vom import bei klick auf ok
 			des subImportDialoges true gesetzt bzw. beim klick auf abbrechen false
 		*/
-		this.kurveDarstellungMenuItem[kurvenNummer - 1].setEnabled(wasnjetz);
-		this.kurveNeuZeichnenMenuItem[kurvenNummer - 1].setEnabled(wasnjetz);
+		this.curveEditable[id] = flag;
 	}
 } // public class MenuBar
