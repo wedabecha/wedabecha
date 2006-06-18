1ed41af9d9033e19a41ca43d9d7da9f20ae6aaf1
Dominic Hopf <dmaphy@googlemail.com>
2006-06-18 10:40:00 +0000

    rewrote the MainToolbar. BUG: the Buttons don't show any pictures.

--- src/de/codeforum/wedabecha/ui/MainToolBar.java
+++ src/de/codeforum/wedabecha/ui/MainToolBar.java
@@ -25,124 +25,161 @@ import javax.swing.*;
 import de.codeforum.wedabecha.wedabecha;
 
 /**
- * @author
- * Robert Exner (ashrak at users.berlios.de),
- * Matthias Tylkowski (micron at users.berlios.de)
+ * @author Robert Exner (ashrak at users.berlios.de)
+ * @author Matthias Tylkowski (micron at users.berlios.de)
+ * @author Dominic Hopf (dmaphy at users.berlios.de)
  */
-public class MainToolBar {
-	private String imagepath =  "./de/codeforum/wedabecha/ui/";
-	protected JToolBar toolBar = new JToolBar("Werkzeugleiste");
-		private ImageIcon oeffnen = new ImageIcon(this.imagepath + "open.gif");
-			private JButton oeffnenbutton = new JButton(this.oeffnen);
-		private ImageIcon speichern = new ImageIcon(this.imagepath + "save.gif");
-			private JButton speichernbutton = new JButton(this.speichern);
-		/*
-			werden erst gebraucht, wenn die Funktion implementiert ist
-		*/
-// 		private ImageIcon undo = new ImageIcon("Images/undo.gif");
-// 			private JButton undobutton = new JButton(undo);
-// 		private ImageIcon redo = new ImageIcon("Images/redo.gif");
-// 			private JButton redobutton = new JButton(redo);
+public class MainToolBar extends JToolBar {
 
-		private ImageIcon gitter = new ImageIcon(this.imagepath + "grid.gif");
-			private JToggleButton gitterbutton = new JToggleButton(this.gitter);
-		private ImageIcon liniezeichnen = new ImageIcon(this.imagepath + "line.gif");
-			protected JToggleButton liniezeichnenbutton = new JToggleButton(this.liniezeichnen);
-		private ImageIcon pfeilzeichnen = new ImageIcon(this.imagepath + "arrow.gif");
-			protected JToggleButton pfeilzeichnenbutton = new JToggleButton(this.pfeilzeichnen);
-		private ImageIcon textzeichnen = new ImageIcon(this.imagepath + "text.gif");
-			protected JToggleButton textzeichnenbutton = new JToggleButton(this.textzeichnen);
-		private JLabel kurveLabel = new JLabel("Kurve: ");
-		protected JToggleButton kurveButton[] = {
-						    new JToggleButton(" 1 "),
-						    new JToggleButton(" 2 "),
-						    new JToggleButton(" 3 "),
-						    new JToggleButton(" 4 "),
-						    new JToggleButton(" 5 ")
-		};
-
-//		private int breite;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	final JToggleButton gridButton = new JToggleButton(new ImageIcon("grid.gif"));
+	final JToggleButton drawLineButton = new JToggleButton(new ImageIcon("line.gif"));
+	final JToggleButton drawArrowButton = new JToggleButton(new ImageIcon("arrow.gif"));
+	final JToggleButton drawTextButton = new JToggleButton(new ImageIcon("text.gif"));
+	
+	final JToggleButton curveButton[] = {
+		    new JToggleButton(" 1 "),
+		    new JToggleButton(" 2 "),
+		    new JToggleButton(" 3 "),
+		    new JToggleButton(" 4 "),
+		    new JToggleButton(" 5 ")
+	};
 
 
-	// Konstruktor
+	/**
+	 * Konstruktor
+	 * @param breite die Breite der Symbolleiste
+	 */
 	public MainToolBar(int breite){
 		this.pack();
-		this.toolBar.setSize(breite, 35);
+		this.setSize(breite, 35);
 	} // toolBarUI()
 
 
 	protected void setBreite(int breite){
-	    this.toolBar.setSize(breite, 35);
+	    this.setSize(breite, 35);
 	}// setBreite(int breite)
 
 
-	// Setzt die ToolBar zusammen
+	/**
+	 * Setzt die ToolBar zusammen
+	 */
 	public void pack(){
-		this.toolBar.add(this.oeffnenbutton);
-			this.oeffnenbutton.setContentAreaFilled(true);
-		this.toolBar.add(this.speichernbutton);
-			this.speichernbutton.setContentAreaFilled(true);
-		/* werden vielleicht noch Implementiert wenn noch Zeit ist */
-		//this.toolBar.add(undobutton);
-			//this.undobutton.setContentAreaFilled(true);
-		//this.toolBar.add(redobutton);
-			//this.redobutton.setContentAreaFilled(true);
-		this.toolBar.addSeparator();
-		this.toolBar.add(this.gitterbutton);
-			this.gitterbutton.setContentAreaFilled(true);
-			this.gitterbutton.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent event) {
-					if (gitterGewaehlt()) {
-						MainWindow.setGridVisibility(false);
-					} else {
-						MainWindow.setGridVisibility(true);
-					}
+		final JButton openButton = new JButton(new ImageIcon("images/open.gif"));
+		openButton.setContentAreaFilled(true);
+		this.add(openButton);
+		
+		final JButton saveButton = new JButton(new ImageIcon("save.gif"));
+		saveButton.setContentAreaFilled(true);
+		this.add(saveButton);
+		
+		this.addSeparator();
+		
+		final JButton undoButton = new JButton(new ImageIcon("undo.gif"));
+		undoButton.setContentAreaFilled(true);
+		undoButton.setEnabled(false);
+		this.add(undoButton);
+		
+		final JButton redoButton = new JButton(new ImageIcon("redo.gif"));
+		redoButton.setContentAreaFilled(true);
+		redoButton.setEnabled(false);
+		this.add(redoButton);
+		
+		this.addSeparator();
+
+		
+		this.gridButton.setContentAreaFilled(true);
+		this.gridButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				if (gitterGewaehlt()) {
+					MainWindow.setGridVisibility(false);
+				} else {
+					MainWindow.setGridVisibility(true);
 				}
-			});
-			this.gitterbutton.setToolTipText("Raster anzeigen");
-		this.toolBar.addSeparator();
-		this.toolBar.add(this.liniezeichnenbutton);
-			this.liniezeichnenbutton.setContentAreaFilled(true);
-			this.liniezeichnenbutton.addActionListener(new linieButtonListener());
-			this.liniezeichnenbutton.setToolTipText("Linie zeichnen");
-		this.toolBar.add(this.pfeilzeichnenbutton);
-			this.pfeilzeichnenbutton.setContentAreaFilled(true);
-			this.pfeilzeichnenbutton.addActionListener(new pfeilButtonListener());
-			this.pfeilzeichnenbutton.setToolTipText("Pfeil zeichnen");
-		this.toolBar.add(this.textzeichnenbutton);
-			this.textzeichnenbutton.setContentAreaFilled(true);
-			this.textzeichnenbutton.addActionListener(new textButtonListener());
-			this.textzeichnenbutton.setToolTipText("Text einf\u00fcgen");
+			}
+		});
+		this.gridButton.setToolTipText("Raster anzeigen");
+		this.add(this.gridButton);
 
-		this.toolBar.addSeparator();
-		this.toolBar.addSeparator();
+		this.addSeparator();
+		
+		this.drawLineButton.setContentAreaFilled(true);
+		this.drawLineButton.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent e) {
+				if(linieGewaehlt()){
+					abwaehlen(3);
+				} // if
+		    } // actionPerformed(ActionEvent e)
+		});
+		
+		this.drawLineButton.setToolTipText("Linie zeichnen");
+		this.add(this.drawLineButton);
+		
+		this.drawArrowButton.setContentAreaFilled(true);
+		this.drawArrowButton.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent e) {
+				if( pfeilGewaehlt() ){
+					abwaehlen(1);
+				} // if
+		    } // actionPerformed(ActionEvent e)
+		});
+		
+		this.drawArrowButton.setToolTipText("Pfeil zeichnen");
+		this.add(this.drawArrowButton);
+	
+		this.drawTextButton.setContentAreaFilled(true);
+		this.drawTextButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			    if(textGewaehlt()){
+					abwaehlen(2);
+			    }//if
+			}// actionPerformed(ActionEvent e)
 
-		this.toolBar.add(this.kurveLabel);
+		});
+		
+		this.drawTextButton.setToolTipText("Text einf\u00fcgen");
+		this.add(this.drawTextButton);
+		
+		this.addSeparator();
+		this.addSeparator();
+		
+		final JLabel curveLabel = new JLabel("Kurve: ");
+		this.add(curveLabel);
+		
 		for(int i = 0; i < 5; i++){
-		    this.toolBar.add(this.kurveButton[i]);
-		    this.kurveButton[i].addActionListener(new kurveButtonListener());
-			this.kurveButton[i].setEnabled(false);
+		    this.add(this.curveButton[i]);
+		    this.curveButton[i].addActionListener(new curveButtonListener());
+			this.curveButton[i].setEnabled(false);
 		}// for
 
-		this.toolBar.addSeparator();
-		this.toolBar.addSeparator();
+		this.addSeparator();
+		this.addSeparator();
 
-		this.toolBar.setFloatable(false);
-		this.toolBar.setBorderPainted(true);
+		this.setFloatable(false);
+		this.setBorderPainted(true);
 	} // pack()
 
-	public JToolBar getToolBar(){
-		return this.toolBar;
-	} // getToolBar()
-
-
+	
+	/**
+	 * Einen Button für eine Kurve einschalten oder ausschalten.
+	 * @param kurve die Nummer der Kurve
+	 * @param janein true oder false, ob der Button anklickbar ist oder nicht.
+	 * TODO: englische Namensgebung.
+	 */
 	public void kurveWaehlen(int kurve, boolean janein){
-		this.kurveButton[kurve-1].setEnabled(janein);
-	}// kurveWaehlen()
+		this.curveButton[kurve-1].setEnabled(janein);
+	} // kurveWaehlen()
 
 
-	class kurveButtonListener implements ActionListener {
-
+	class curveButtonListener implements ActionListener {
+		/**
+		 * contains commands to execute if an Action is performed.
+		 * @param e ActionEvent
+		 */
 	    public void actionPerformed(ActionEvent e) {
 			try{
 				for(int i = 0; i < 5; i++){
@@ -152,155 +189,166 @@ public class MainToolBar {
 						 * Ich hätte an sowas wie parent.objekt gedacht,
 						 * ist es aber nicht.
 						 */
-						if(kurveButton[i].isSelected()){
-							if(wedabecha.getCurveByID(i + 1).getStyleIndex() == 0){
+						if (curveButton[i].isSelected()) {
+							if (wedabecha.getCurveByID(i + 1).getStyleIndex() == 0) {
 								wedabecha.getCurveByID(i + 1).getShareCurve().setVisibility(true);
-							}else{
+							} else {
 								wedabecha.getCurveByID(i + 1).getLineCurve().setVisibility(true);
 							} // if()-else
-						}else{
-							if(wedabecha.getCurveByID(i + 1).getStyleIndex() == 0){
+						} else {
+							if (wedabecha.getCurveByID(i + 1).getStyleIndex() == 0){
 								wedabecha.getCurveByID(i + 1).getShareCurve().setVisibility(false);
-							}else{
+							} else {
 								wedabecha.getCurveByID(i + 1).getLineCurve().setVisibility(false);
 							} // if()-else
 						} // if()-else
 					} // if()
 				} // for()
-			}catch(NullPointerException exception){
-			JOptionPane.showMessageDialog( null, "Sie haben vergessen, die Kurve neu zeichnen zu lassen.\n"+
-											"Klicken sie auf [Kurve] --> [Kurvennummer] --> [Neu Zeichnen]" );
+			} catch(NullPointerException exception) {
+				JOptionPane.showMessageDialog (
+						null, "Sie haben vergessen, die Kurve neu zeichnen zu lassen.\n" +
+								"Klicken sie auf [Kurve] --> [Kurvennummer] --> [Neu Zeichnen]"
+				);
 			}
 	    } // actionPerformed()
 
-	}// kurveButtonListener
+	} // curveButtonListener
 
 
-	class linieButtonListener implements ActionListener{
-
-	    public void actionPerformed(ActionEvent e) {
-			if(linieGewaehlt()){
-				abwaehlen(3);
-			}// if
-	    }// actionPerformed(ActionEvent e)
-
-	}// linienButtonListener
 
 
-	class pfeilButtonListener implements ActionListener{
-
-	    public void actionPerformed(ActionEvent e) {
-			if(pfeilGewaehlt()){
-				abwaehlen(1);
-			}// if
-	    }// actionPerformed(ActionEvent e)
-
-	}// pfeilButtonListener
-
-
-	class textButtonListener implements ActionListener{
-
-		public void actionPerformed(ActionEvent e) {
-		    if(textGewaehlt()){
-				abwaehlen(2);
-		    }//if
-		}// actionPerformed(ActionEvent e)
-
-	}// textButtonListener
-
-	/* Diese Methode sorgt dafür, dass man nur einen von den 3 Buttons (Text,
+	/**
+	 * Diese Methode sorgt dafür, dass man nur einen von den 3 Buttons (Text,
 	 * Line oder Pfeil) gleichzeitig anwählen kann
+	 * @param ein Die Nummer des Buttons, der eingeschaltet wird.
 	 */
-
-	public void abwaehlen(int aus){
-	    switch(aus){
+	public void abwaehlen(int ein){
+	    switch(ein){
 			case 1:
-				this.textzeichnenbutton.setSelected(false);
-				this.liniezeichnenbutton.setSelected(false);
+				this.drawTextButton.setSelected(false);
+				this.drawLineButton.setSelected(false);
 				break;
 			case 2:
-				this.liniezeichnenbutton.setSelected(false);
-				this.pfeilzeichnenbutton.setSelected(false);
+				this.drawLineButton.setSelected(false);
+				this.drawArrowButton.setSelected(false);
 				break;
 			case 3:
-			    this.pfeilzeichnenbutton.setSelected(false);
-			    this.textzeichnenbutton.setSelected(false);
+			    this.drawArrowButton.setSelected(false);
+			    this.drawTextButton.setSelected(false);
 			    break;
-	    }// switch
-	}// abwählen()
+	    } // switch
+	} // abwählen()
 
-	// get-Methoden für die jeweiligen Buttons in der Toolbar
+	
+	/**
+	 * liefert zurück, ob der Button zum Zeichnen von Text gedrückt is
+	 * @return true or false
+	 */
 	public boolean textGewaehlt(){
-	    return this.textzeichnenbutton.isSelected();
-	}// textGewählt()
+	    return this.drawTextButton.isSelected();
+	} // textGewaehlt()
 
-
+	
+	/**
+	 * liefert zurück, ob der Button zum Gitter anzeigen gedrückt ist
+	 * @return true or false
+	 */
 	public boolean gitterGewaehlt(){
-	    return this.gitterbutton.isSelected();
-	}// gitterGewaehlt()
+	    return this.gridButton.isSelected();
+	} // gitterGewaehlt()
 
-
+	
+	/**
+	 * liefert zurück, ob der Button zum Zeichnen eines Pfeils gedrückt ist
+	 * @return true or false
+	 */
 	public boolean pfeilGewaehlt(){
-	    return this.pfeilzeichnenbutton.isSelected();
-	}// pfeilGewaehlt()
+	    return this.drawArrowButton.isSelected();
+	} // pfeilGewaehlt()
 
-
+	
+	/**
+	 * liefert zurück ob der Button zum Zeichnen einer Linie gedrückt ist.
+	 * @return true or false
+	 */
 	public boolean linieGewaehlt(){
-	    return this.liniezeichnenbutton.isSelected();
-	}// linieGewaehlt
-
-	/* Diese Methode sorgt dafür, dass die erste Kurve, die importiert wird auch
-	 * gleich angezeigt wird, damit man sieht, dass alles geklappt hat
+	    return this.drawLineButton.isSelected();
+	} // linieGewaehlt()
+	
+	/*
+	 * TODO eine Methode schreiben, welche die Aufgabe aller fünf folgenden
+	 * Methoden übernimmt.
 	 */
 
+	/**
+	 * Diese Methode sorgt dafür, dass die erste Kurve, die importiert wird auch
+	 * gleich angezeigt wird, damit man sieht, dass alles geklappt hat
+	 */
 	public void setKurve1Button(){
-		this.kurveButton[0].setSelected(true);
-		if(wedabecha.getCurveByID(1).getStyleIndex() == 0){
+		this.curveButton[0].setSelected(true);
+		
+		if (wedabecha.getCurveByID(1).getStyleIndex() == 0) {
 			wedabecha.getCurveByID(1).getShareCurve().setVisibility(true);
-		}else{
+		} else {
 			wedabecha.getCurveByID(1).getLineCurve().setVisibility(true);
 		} // if()-else
 	} // setKurve1Button()
 
-
+	/**
+	 * Diese Methode sorgt dafür, dass die erste Kurve, die importiert wird auch
+	 * gleich angezeigt wird, damit man sieht, dass alles geklappt hat
+	 */
 	public void setKurve2Button(){
-		this.kurveButton[1].setSelected(true);
-		if(wedabecha.getCurveByID(2).getStyleIndex() == 0){
+		this.curveButton[1].setSelected(true);
+		
+		if (wedabecha.getCurveByID(2).getStyleIndex() == 0) {
 			wedabecha.getCurveByID(2).getShareCurve().setVisibility(true);
-		}else{
+		} else {
 			wedabecha.getCurveByID(2).getLineCurve().setVisibility(true);
 		} // if()-else
 	} // setKurve1Button()
 
-
+	/**
+	 * Diese Methode sorgt dafür, dass die erste Kurve, die importiert wird auch
+	 * gleich angezeigt wird, damit man sieht, dass alles geklappt hat
+	 */
 	public void setKurve3Button(){
-		this.kurveButton[2].setSelected(true);
-		if(wedabecha.getCurveByID(3).getStyleIndex() == 0){
+		this.curveButton[2].setSelected(true);
+		
+		if (wedabecha.getCurveByID(3).getStyleIndex() == 0) {
 			wedabecha.getCurveByID(3).getShareCurve().setVisibility(true);
-		}else{
+		} else {
 			wedabecha.getCurveByID(3).getLineCurve().setVisibility(true);
 		} // if()-else
 	} // setKurve1Button()
 
-
+	/**
+	 * Diese Methode sorgt dafür, dass die erste Kurve, die importiert wird auch
+	 * gleich angezeigt wird, damit man sieht, dass alles geklappt hat
+	 */
 	public void setKurve4Button(){
-		this.kurveButton[3].setSelected(true);
-		if(wedabecha.getCurveByID(4).getStyleIndex() == 0){
+		this.curveButton[3].setSelected(true);
+		
+		if (wedabecha.getCurveByID(4).getStyleIndex() == 0) {
 			wedabecha.getCurveByID(4).getShareCurve().setVisibility(true);
-		}else{
+		} else {
 			wedabecha.getCurveByID(4).getLineCurve().setVisibility(true);
 		} // if()-else
 	} // setKurve1Button()
 
-
+	/**
+	 * Diese Methode sorgt dafür, dass die erste Kurve, die importiert wird auch
+	 * gleich angezeigt wird, damit man sieht, dass alles geklappt hat
+	 */
 	public void setKurve5Button(){
-		this.kurveButton[4].setSelected(true);
-		if(wedabecha.getCurveByID(5).getStyleIndex() == 0){
+		this.curveButton[4].setSelected(true);
+		
+		if (wedabecha.getCurveByID(5).getStyleIndex() == 0) {
 			wedabecha.getCurveByID(5).getShareCurve().setVisibility(true);
-		}else{
+		} else {
 			wedabecha.getCurveByID(5).getLineCurve().setVisibility(true);
 		} // if()-else
 	} // setKurve1Button()
 
 
-} // toolBarUI
+} // MainToolBar
